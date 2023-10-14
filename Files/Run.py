7c605e369d3a2c4e29fc9506cf4bed1386cfce9d
Mashreghi <amiramirmashgh@gmail.com>
2023-10-14 15:21:36 +0330

    ✅ 2023-10-14 15:21:36 ✅

---
+++ Files/Run.py
@@ -0,0 +1,26 @@
+import git
+from datetime import datetime
+
+# Specify the path to your Git repository
+repo_path = r'H:\GIT project\free-config-collector'
+
+# Initialize the Git repository
+repo = git.Repo(repo_path)
+
+# Check for any changes in the working directory
+if repo.is_dirty():
+    # Stage all changes
+    repo.index.add('*')
+
+    # Create a commit message with the current date and time and ✅ emoji
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    commit_message = f"✅ {timestamp} ✅"
+    repo.index.commit(commit_message)
+
+    # Push the changes to the origin
+    origin = repo.remotes['origin']  # Replace 'origin' with the name of your remote
+    origin.push()
+
+    print("Changes committed and pushed to the origin.")
+else:
+    print("No changes to commit.")
