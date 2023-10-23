f7ba257056fd3b56619ec207a35577887ce15ee3
Mashreghi <78053151+M-Mashreghi@users.noreply.github.com>
2023-10-23 09:24:55 +0330

    remove junk files

--- Files/test.py
+++
@@ -1,9 +0,0 @@
-import pycountry
-
-input_countries = ['American Samoa', 'Canada', 'France']
-
-countries = {}
-for country in pycountry.countries:
-    countries[country.name] = country.alpha_2
-
-print(countries)
